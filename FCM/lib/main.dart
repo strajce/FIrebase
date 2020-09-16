489bc0f1e155be58916e7a9767f93b4d4deaa9b1
s.vuckovic <s.vuckovic@levi9.com>
2020-09-16 12:16:41 +0200

    Add basic auth screen

--- FCM/lib/main.dart
+++ FCM/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:FCM/screens/auth_screen.dart';
 import 'package:FCM/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ChatScreen(),
+      // home: ChatScreen(),
+      home: AuthScreen(),
     );
   }
 }
--- FCM/lib/screens/chat_screen.dart
+++ FCM/lib/screens/chat_screen.dart
@@ -5,25 +5,49 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => (Container(
-          padding: EdgeInsets.all(8),
-          child: Text('That\'s works!!'),
-        )),
-      ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/xvau2lLhvpDFWMFwNZBY/messages')
+            .snapshots(),
+        builder: (context, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final document = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: document.length,
+            itemBuilder: (context, index) => (Container(
+              padding: EdgeInsets.all(8),
+              child: Text(document[index]['text']),
+            )),
+          );
+        },
+      )
+      //         ListView.builder(
+      //   itemCount: 10,
+      //   itemBuilder: (context, index) => (Container(
+      //     padding: EdgeInsets.all(8),
+      //     child: Text('That\'s works!!'),
+      //   )),
+      // ),
+      ,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
+          // Firestore.instance
+          //     .collection('chats/xvau2lLhvpDFWMFwNZBY/messages')
+          //     .snapshots()
+          //     .listen((event) {
+          // print(event.documents[0]['text']);
+          // event.documents.forEach((element) {
+          // print(element['text']);
+          // });
+          // });
           Firestore.instance
               .collection('chats/xvau2lLhvpDFWMFwNZBY/messages')
-              .snapshots()
-              .listen((event) {
-            // print(event.documents[0]['text']);
-            event.documents.forEach((element) {
-              print(element['text']);
-            });
-          });
+              .add({'text': 'This text was added but clicking on button!'});
         },
       ),
     );
